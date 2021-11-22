f2aa33ea4304e033e5d4f0fb97111958bc4e9a34
deepakcodefreak <deepak19.engg@gmail.com>
2021-11-22 20:15:30 +0530

    safely handling untrusted code by using iframes

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import {fetchPlugin} from "./plugins/fetchPlugin"
 const App = ()=>{
 
     const ref = useRef<any>()
+    const iframe = useRef<any>()
     const [text,setText] = useState("")
-    const [code,setCode] = useState("")
 
     const startService = async()=>{
         const service = await esbuild.startService({
@@ -23,8 +23,32 @@ const App = ()=>{
         startService()
     },[])
 
+    const html  = `
+    <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message',(event)=>{
+                   try{ 
+                      eval(event.data)
+                   }catch(e){
+                      const root = document.getElementById('root')
+                      root.innerHTML = '<div style="color:red"><h4>Runtime Error</h4>' + e + '</div>'  
+                      console.error(e)  
+                    }
+                },false)
+            </script>
+        </body>
+    <html>
+    `
+
+
     const onClick = async ()=>{
         if(!ref.current) return
+
+        iframe.current.srcdoc = html
+
        const result = await ref.current.build({
            entryPoints:['index.js'],
            bundle:true,
@@ -37,9 +61,11 @@ const App = ()=>{
                global:'window'
            }
        }) 
-       console.log(result.outputFiles[0].text)
-       setCode(result.outputFiles[0].text)
+      
+    
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text,'*')
     }
+
 
 
    return (
@@ -47,10 +73,13 @@ const App = ()=>{
            <textarea value={text} onChange={(e)=>setText(e.target.value)}></textarea>
            <div>
                <button onClick={onClick}>Submit</button>
-               <pre>{code}</pre>
+               <pre></pre>
+               <iframe title="code preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
            </div>
        </div>
    ) 
 }
+
+
 
 ReactDOM.render(<App/>,document.getElementById("root"))
