429316bdfd7e252f3eb8a21e514b0879d0af4d9f
deepakcodefreak <deepak19.engg@gmail.com>
2021-11-22 12:51:25 +0530

    support for importing css files added

--- src/plugins/fetchPlugin.ts
+++ src/plugins/fetchPlugin.ts
@@ -20,10 +20,22 @@ export const fetchPlugin = (inputCode:string)=>{
                 if(cachedFile) return cachedFile
         
                 const {data,request} = await axios.get(args.path)
-                
+                const fileType  = args.path.match(/.css$/)?'css':'jsx'
+
+                const escapedContent = data
+                .replace(/\n/g,'')
+                .replace(/"/g,'\\"')
+                .replace(/'/g,"\\'")
+
+                const contents = fileType==="css"?`
+                  const style = document.createElement('style')
+                  style.innerText = '${escapedContent}'
+                  document.head.appendChild(style)
+                `:data
+                console.log(contents)
                 const result:esbuild.OnLoadResult = await  { 
-                    loader:'jsx',
-                    contents: data,
+                    loader: 'jsx',
+                    contents,
                     resolveDir: new URL('./',request.responseURL).pathname
                 }
                 await localForage.setItem(args.path,result)
