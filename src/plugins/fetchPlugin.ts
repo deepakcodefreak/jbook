4dd8131bdeadc2591191d4689664dd269a311332
deepakcodefreak <deepak19.engg@gmail.com>
2021-11-22 13:19:04 +0530

    fetchPlugin refractored to make it more cleaner

--- src/plugins/fetchPlugin.ts
+++ src/plugins/fetchPlugin.ts
@@ -6,36 +6,57 @@ export const fetchPlugin = (inputCode:string)=>{
     return {
         name:'fetch-plugin',
         setup(build:esbuild.PluginBuild){
-            build.onLoad({ filter: /.*/ ,namespace: 'a'}, async (args: any) => {
-                console.log('onLoad', args);
-         
-                if (args.path === 'index.js') {
-                  return {
-                    loader: 'jsx',
-                    contents: inputCode,
-                  };
-                } 
-        
-                const cachedFile = await localForage.getItem<esbuild.OnLoadResult>(args.path)
-                if(cachedFile) return cachedFile
+
+            build.onLoad({ filter:/(^index\.js$)/},()=>{
+              return {
+                loader: 'jsx',
+                contents: inputCode,
+              };
+            })
+
+          build.onLoad({ filter:/.*/},async (args:any)=>{
+            const cachedFile = await localForage.getItem<esbuild.OnLoadResult>(args.path)
+            if(cachedFile) return cachedFile
+  
+          })
+
+            build.onLoad( { filter:/.css$/ },async (args:any)=>{
+              // const cachedFile = await localForage.getItem<esbuild.OnLoadResult>(args.path)
+              //   if(cachedFile) return cachedFile
         
                 const {data,request} = await axios.get(args.path)
-                const fileType  = args.path.match(/.css$/)?'css':'jsx'
+  
 
                 const escapedContent = data
                 .replace(/\n/g,'')
                 .replace(/"/g,'\\"')
                 .replace(/'/g,"\\'")
 
-                const contents = fileType==="css"?`
-                  const style = document.createElement('style')
+                const contents = 
+                  `const style = document.createElement('style')
                   style.innerText = '${escapedContent}'
                   document.head.appendChild(style)
-                `:data
+                `
                 console.log(contents)
                 const result:esbuild.OnLoadResult = await  { 
                     loader: 'jsx',
                     contents,
+                    resolveDir: new URL('./',request.responseURL).pathname
+                }
+                await localForage.setItem(args.path,result)
+                return result
+            })
+           
+            build.onLoad({ filter: /.*/ ,namespace: 'a'}, async (args: any) => {
+                
+                // const cachedFile = await localForage.getItem<esbuild.OnLoadResult>(args.path)
+                // if(cachedFile) return cachedFile
+        
+                const {data,request} = await axios.get(args.path)
+          
+                const result:esbuild.OnLoadResult = await  { 
+                    loader: 'jsx',
+                    contents:data,
                     resolveDir: new URL('./',request.responseURL).pathname
                 }
                 await localForage.setItem(args.path,result)
