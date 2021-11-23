19d761bd8ba8bb07001268571eb1f2202fd5bf13
deepakcodefreak <deepak19.engg@gmail.com>
2021-11-23 14:02:10 +0530

    bundling logic moved to seperate life

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,19 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from "react-dom";
-import { useState, useEffect, useRef } from "react";
-import * as esbuild from "esbuild-wasm"
-import { unpkgPathPlugin } from "./plugins/unpkg-path-pugin";
-import {fetchPlugin} from "./plugins/fetchPlugin"
+import { useState } from "react";
 import CodeEditor from "./components/code-editor"
 import Preview from "./components/Preview"
+import bundle from "./bundler"
 
 const App = ()=>{
 
     const [text,setText] = useState("")
     const [code,setCode] = useState("")
 
-    const ref = useRef<any>()
-
-
-    const startService = async()=>{
-        const service = await esbuild.startService({
-            worker:true,
-            wasmURL:"https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm"
-        })
-        ref.current = service
-    }
-
-
-    useEffect(()=>{
-        startService()
-    },[])
-
-   
-
     const onClick = async ()=>{
-        if(!ref.current) return
-
-        // iframe.current.srcdoc = html
-
-       const result = await ref.current.build({
-           entryPoints:['index.js'],
-           bundle:true,
-           write:false,
-           plugins:[
-               unpkgPathPlugin(),
-               fetchPlugin(text)],
-           define:{
-               'process.env.NODE_ENV':'"production"',
-               global:'window'
-           }
-       }) 
-       setCode(result.outputFiles[0].text)
-    
+       const output = await bundle(text) 
+       setCode(output)
     }
-
-
 
    return (
        <div className="editor-wrapper">
