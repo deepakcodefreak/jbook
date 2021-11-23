60c67d85723d81083c07f83ed6ef8286759eee97
deepakcodefreak <deepak19.engg@gmail.com>
2021-11-23 11:22:44 +0530

    iframe moved to a seperate Component called Preview

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import * as esbuild from "esbuild-wasm"
 import { unpkgPathPlugin } from "./plugins/unpkg-path-pugin";
 import {fetchPlugin} from "./plugins/fetchPlugin"
 import CodeEditor from "./components/code-editor"
+import Preview from "./components/Preview"
 
 const App = ()=>{
 
-    const ref = useRef<any>()
-    const iframe = useRef<any>()
     const [text,setText] = useState("")
+    const [code,setCode] = useState("")
+
+    const ref = useRef<any>()
+
 
     const startService = async()=>{
         const service = await esbuild.startService({
@@ -25,31 +28,12 @@ const App = ()=>{
         startService()
     },[])
 
-    const html  = `
-    <html>
-        <head></head>
-        <body>
-            <div id="root"></div>
-            <script>
-                window.addEventListener('message',(event)=>{
-                   try{ 
-                      eval(event.data)
-                   }catch(e){
-                      const root = document.getElementById('root')
-                      root.innerHTML = '<div style="color:red"><h4>Runtime Error</h4>' + e + '</div>'  
-                      console.error(e)  
-                    }
-                },false)
-            </script>
-        </body>
-    <html>
-    `
-
+   
 
     const onClick = async ()=>{
         if(!ref.current) return
 
-        iframe.current.srcdoc = html
+        // iframe.current.srcdoc = html
 
        const result = await ref.current.build({
            entryPoints:['index.js'],
@@ -63,9 +47,8 @@ const App = ()=>{
                global:'window'
            }
        }) 
-      
+       setCode(result.outputFiles[0].text)
     
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text,'*')
     }
 
 
@@ -79,8 +62,7 @@ const App = ()=>{
            <textarea value={text} onChange={(e)=>setText(e.target.value)}></textarea>
            <div>
                <button onClick={onClick}>Submit</button>
-               <pre></pre>
-               <iframe title="code preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+               <Preview code={code}/>
            </div>
        </div>
    ) 
